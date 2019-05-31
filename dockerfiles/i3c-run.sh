29719f8556d2e089be2c6f395c2a0a16fb55edad
i3c-cloud <42610398+i3c-cloud@users.noreply.github.com>
2019-05-31 17:52:26 +0200

    Update i3c-run.sh
    run

---
+++ dockerfiles/i3c-run.sh
@@ -0,0 +1,12 @@
+
+addIParams=true;
+dParams="-d --privileged --hostname terminalserver --shm-size 1g -p 3389:3389 -p 2222:22 \
+	$addParams \
+	-v $uData/home:/home \
+	-v $uData/etc/ssh:/etc/ssh"
+	
+	
+i3cAfter(){
+	
+	/i exshd xrdp-mint "su - mb && npm install -g gulp hjson";
+}	
